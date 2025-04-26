295ffe889ddd308bd45474e781839603d9c98a39
Ricardo Gomez <r.gomez1999@outlook.com>
2025-04-25 21:34:08 -0600

    timer page

---
+++ app/login/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const res = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email }),
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      setError(data.message || "El Login fallo");
+    } else {
+      localStorage.setItem("voter", JSON.stringify(data.candidate));
+      router.push("/vote");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center item bg-orange-900 ">
+      <div className="max-w-2xl h-96  w-full flex flex-col md:flex-row items-center bg-black/50 rounded-3xl">
+        <div className=" bg-[#F8873A]/90 h-full w-full flex justify-center items-center md:rounded-l-3xl rounded-t-3xl md:rounded-t-none">
+          <Image src={"/retomamerto.png"} alt="logo" width={150} height={150} />
+        </div>
+        <form
+          onSubmit={handleLogin}
+          className=" w-full flex flex-col items-center justify-center gap-5 h-full p-5"
+        >
+          <h1 className="text-2xl font-bold mb-4 text-white">
+            Ingresa para votar
+          </h1>
+          <input
+            type="email"
+            placeholder="Ingresa tu email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className="w-full px-4 py-2 mb-2 border rounded"
+          />
+          {error && <p className="text-orange-900 text-sm mb-2">{error}</p>}
+          <button className="w-full bg-[#F8873A] text-white py-2 rounded hover:bg-[#F8873A]/80 font-bold  cursor-pointer">
+            Empezar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,52 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
-export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [error, setError] = useState("");
+export default function HomePage() {
   const router = useRouter();
+  const [timeLeft, setTimeLeft] = useState<number>(0);
 
-  const handleLogin = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const targetDate = new Date();
+  targetDate.setDate(targetDate.getDate() + 1); // Tomorrow
+  targetDate.setHours(14, 0, 0, 0); // 2 PM
 
-    const res = await fetch("/api/auth/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email }),
-    });
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = targetDate.getTime() - now;
 
-    const data = await res.json();
+      if (distance <= 0) {
+        clearInterval(interval);
+        router.push("/login");
+      } else {
+        setTimeLeft(distance);
+      }
+    }, 1000);
 
-    if (!res.ok) {
-      setError(data.message || "El Login fallo");
-    } else {
-      localStorage.setItem("voter", JSON.stringify(data.candidate));
-      router.push("/vote");
-    }
+    return () => clearInterval(interval);
+  }, [router, targetDate]);
+
+  const formatTime = (milliseconds: number) => {
+    const totalSeconds = Math.floor(milliseconds / 1000);
+    const hours = Math.floor((totalSeconds % (60 * 60 * 24)) / (60 * 60));
+    const minutes = Math.floor((totalSeconds % (60 * 60)) / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+
+    return `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center item bg-orange-900 ">
-      <div className="max-w-2xl h-96  w-full flex flex-col md:flex-row items-center bg-black/50 rounded-3xl">
-        <div className=" bg-[#F8873A]/90 h-full w-full flex justify-center items-center md:rounded-l-3xl rounded-t-3xl md:rounded-t-none">
-          <Image src={"/retomamerto.png"} alt="logo" width={150} height={150} />
-        </div>
-        <form
-          onSubmit={handleLogin}
-          className=" w-full flex flex-col items-center justify-center gap-5 h-full p-5"
-        >
-          <h1 className="text-2xl font-bold mb-4 text-white">
-            Ingresa para votar
-          </h1>
-          <input
-            type="email"
-            placeholder="Ingresa tu email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="w-full px-4 py-2 mb-2 border rounded"
-          />
-          {error && <p className="text-orange-900 text-sm mb-2">{error}</p>}
-          <button className="w-full bg-[#F8873A] text-white py-2 rounded hover:bg-[#F8873A]/80 font-bold  cursor-pointer">
-            Empezar
-          </button>
-        </form>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-orange-900 text-white">
+      <Image src={"/retomamerto.png"} alt="logo" width={150} height={150} />
+      <h1 className="text-3xl md:text-4xl lg:text-6xl text-center font-bold mb-8">
+        ¡Votaciones Reto Mamerto!
+      </h1>
+      <div className="flex text-3xl md:text-4xl lg:text-6xl font-mono">
+        <p>Falta:</p>
+        {formatTime(timeLeft)}
       </div>
     </div>
   );
