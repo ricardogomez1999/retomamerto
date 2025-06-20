ab3f371f57400dddcb030ed93687692fe87dc9e2
Ricardo Gomez <r.gomez1999@outlook.com>
2025-06-19 21:34:11 -0600

    fixing-armando-wha

--- app/components/BrandsBanner.tsx
+++ app/components/BrandsBanner.tsx
@@ -21,12 +21,12 @@ export default function BrandsBanner() {
             />
           ))}
         </div>
-        <Link
+        <a
           href={"/https://wa.me/528443506065"}
           className=" mt-10 border-2 font-bold text-orange-400 border-orange-400 p-3 w-fit m-auto flex justify-center"
         >
           Hazte colaborador
-        </Link>
+        </a>
       </div>
     </div>
   );
